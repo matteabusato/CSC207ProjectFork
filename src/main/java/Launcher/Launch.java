7379fe5b94cc21d7249c55457bb31c92fb236ca9
zeyu cai <zeyucai123456789@gmail.com>
2024-11-23 12:41:38 -0500

    deleted unecessary files

--- src/main/java/Launcher/Launch.java
+++ src/main/java/Launcher/Launch.java
@@ -1,19 +1,11 @@
-package main.java.Launcher;
-
-import main.java.Transaction.TransactionButton;
-import main.java.Transaction.TransactionFrame;
-import main.java.Views.LabelMaker;
-import main.java.Views.PanelMaker;
+package Launcher;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Launch {
 
     public static void main(String[] args) {
 
-        JFrame transaction = new TransactionFrame();
-        transaction.setVisible(true);
     }
 
 }
